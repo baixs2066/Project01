8f7f778015caeacf7e22e532f5acc99160bcdc71
baixs <bai>
2021-04-30 18:50:18 +0800

    1

---
+++ Module01/src/com/bai/bean/Customer.java
@@ -0,0 +1,38 @@
+package com.bai.bean;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+//单行注释
+public class Customer {
+    public static void main(String[] args) {
+        ArrayList list  = new ArrayList();
+//        list.add();
+        System.out.println("hello world");
+    }
+
+    public void method1(){
+
+        Date date = new Date();
+        System.out.println("");
+
+        ArrayList list  = new ArrayList();
+        list.add(123);
+        list.add(234);
+        list.add(444);
+        for (Object o : list) {
+
+        }
+        if (list == null) {
+
+        }
+        if (list != null) {
+
+        }
+
+
+
+
+
+    }
+}
---
+++ Module01/src/com/bai/bean/Customer2.java
@@ -0,0 +1,8 @@
+package com.bai.bean;
+
+/**
+ * @author bxs
+ * @create 2021-04-29 16:32
+ */
+public class Customer2 {
+}
